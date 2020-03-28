04a7af00d3d3ae8e04e1f9551bcd47716239a1dc
Ilya Kharabet <ilyakharabet@gmail.com>
2020-03-28 07:59:29 +0300

    Create DetectItUI module

---
+++ DetectIt/DetectItUI/DetectItUI/Support files/DetectItUI.h
@@ -0,0 +1,19 @@
+//
+//  DetectItUI.h
+//  DetectItUI
+//
+//  Created by Илья Харабет on 28/03/2020.
+//  Copyright © 2020 Mesterra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DetectItUI.
+FOUNDATION_EXPORT double DetectItUIVersionNumber;
+
+//! Project version string for DetectItUI.
+FOUNDATION_EXPORT const unsigned char DetectItUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetectItUI/PublicHeader.h>
+
+
