81a42bbbdb2e0b85171dc10ae6fafa162716fe63
Ilya Kharabet <ilyakharabet@gmail.com>
2020-04-21 15:39:28 +0300

    Add playground for decoder task generation

---
+++ DetectIt/Playgrounds/DecoderTaskCreation.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+@testable import DetectItCore
+
+let task = DecoderTask(
+    id: "",
+    title: "",
+    preposition:
+    """
+    
+    """
+    ,
+    difficulty: 1,
+    score: 1,
+    encodedPictureName: "encoded.jpg",
+    answer: .init(
+        crimeDescription:
+        """
+        
+        """
+        ,
+        decodedMessage: "",
+        possibleAnswers: nil
+    )
+)
+
+let encodedTask = try! JSONEncoder().encode(task)
+let json = String(data: encodedTask, encoding: .utf8)!
+
+print(json)
+
+
+/*
+let task = DecoderTask(
+    id: "",
+    title: "",
+    preposition:
+    """
+    
+    """
+    ,
+    difficulty: 1,
+    score: 1,
+    encodedPictureName: "encoded.jpg",
+    answer: .init(
+        crimeDescription:
+        """
+        
+        """
+        ,
+        decodedMessage: "",
+        possibleAnswers: nil
+    )
+)
+*/
